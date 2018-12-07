c98a1dcfb820d933b0f4b7df2d647cbb2d46b08a
Kyle <oconnork@protonmail.com>
2018-12-06 21:52:46 -0500

    initial commit

---
+++ app/src/main/java/com/example/makingit/softchatapp/TextActivity.java
@@ -0,0 +1,18 @@
+package com.example.makingit.softchatapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by makingit on 11/12/18.
+ */
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+    }
+
+}
