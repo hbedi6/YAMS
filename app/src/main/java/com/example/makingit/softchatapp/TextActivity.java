4d5d72d4a16da043d50a14bce477cf86aaf535bf
hbedi6 <45741465+hbedi6@users.noreply.github.com>
2018-12-09 23:13:45 -0500

    In TextActivity class added intents for submit and userlist, sowhen you click the button, then you go to that activity.

--- app/src/main/java/com/example/makingit/softchatapp/TextActivity.java
+++ app/src/main/java/com/example/makingit/softchatapp/TextActivity.java
@@ -1,7 +1,9 @@
 package com.example.makingit.softchatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by makingit on 11/12/18.
@@ -15,5 +17,14 @@ public class TextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_text);
     }
 
+    public void settings (View view){
+        Intent goToSettings = new Intent(this, SettingActivity2.class);
+        startActivity(goToSettings);
+    }
+
+    public void userList (View view){
+        Intent goToUserList = new Intent(this, UserList.class);
+        startActivity(goToUserList);
+    }
 
 }
